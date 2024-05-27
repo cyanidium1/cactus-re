4f70152628205d388ae880acb750941c49af3cdc
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-05-27 05:37:31 +0200

    get to page.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -63,8 +63,6 @@ export default function Home() {
             skip
         };
 
-        console.log(req);
-
         try {
             const data = await performRequest({ req });
             setPortfolioPosts(data.allProperties);
