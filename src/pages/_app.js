7db6e4dfd093ea833b325ba52a4e4060c610891f
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-06-01 00:12:17 +0200

    fix translation in search

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 // "use client";
-import '../app/globals.css'
-import '../components/TopImage.css'
+import "../app/globals.css";
+import "../components/TopImage.css";
 
 // import '../components/loader.css'
 
@@ -8,13 +8,13 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <NextUIProvider>
-            <NextThemesProvider attribute="class" defaultTheme="dark">
-                <Component {...pageProps} />
-            </NextThemesProvider>
-        </NextUIProvider>
-    )
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
 
 export default MyApp;
