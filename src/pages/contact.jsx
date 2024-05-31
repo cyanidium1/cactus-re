1aa5a20715d5326c04da594eb11224cbe877b670
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-05-31 21:24:17 +0200

    fixing styles for dark

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -18,8 +18,8 @@ function contact(props) {
             />
           </div>
           <div className="lg:col-span-5 md:col-span-6 flex items-center justify-center">
-            <div className="bg-white dark:bg-slate-900 border rounded-md shadow dark:shadow-gray-700 p-6 w-[350px] sm:items-center sm:justify-center">
-              <h3 className="mb-6 text-2xl leading-normal font-medium">
+            <div className="bg-white dark:bg-slate-900 border dark:border-none rounded-md shadow dark:shadow-green-700 p-6 w-[350px] sm:items-center sm:justify-center">
+              <h3 className="mb-6 text-2xl leading-normal font-medium dark:text-white">
                 Get in touch !
               </h3>
               <RequestForm />
