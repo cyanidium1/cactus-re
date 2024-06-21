478ac3d200f183f018882c3d69d48ebec50e429a
Andrii Shtohryn <andriishtohryn@gmail.com>
2024-06-21 18:03:54 +0300

    add sanity

---
+++ sanity/schemaTypes/sellOrRent.tsx
@@ -0,0 +1,6 @@
+export default {
+  name: 'sellOrRent',
+  title: 'Sell Or Rent',
+  type: 'document',
+  fields: [{name: 'value', title: 'Sell Or Rent', type: 'string'}],
+}
---
+++ sanity/schemaTypes/typeOfProperty.tsx
@@ -0,0 +1,6 @@
+export default {
+  name: 'typeOfProperty',
+  title: 'Type Of Property',
+  type: 'document',
+  fields: [{name: 'value', title: 'Type Of Property Name', type: 'string'}],
+}
