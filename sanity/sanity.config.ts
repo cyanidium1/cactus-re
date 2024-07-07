98df7c583ea25c92558e40d297b5d5a1db18ffe1
Andrii Shtohryn <andriishtohryn@gmail.com>
2024-07-07 12:33:06 +0300

    update version sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-
 const SANITY_STUDIO_PROJECT_ID = process.env.SANITY_STUDIO_PROJECT_ID as string
 
 export default defineConfig({
