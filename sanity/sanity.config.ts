e079a20b1411e7974337a90d356ae726b1702c10
Andrii Shtohryn <andriishtohryn@gmail.com>
2024-07-08 08:52:35 +0300

    update scroll

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import './styles.css'
 // import {theme} from 'https://themer.sanity.build/api/hues?preset=dew'
 // const {createTheme, hues} = await import('https://themer.sanity.build/api/hues?preset=dew')
 const SANITY_STUDIO_PROJECT_ID = process.env.SANITY_STUDIO_PROJECT_ID as string
