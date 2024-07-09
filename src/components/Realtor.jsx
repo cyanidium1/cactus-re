2d8656506aef1da99cd8a3d268104ee4acd21dfb
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-07-09 16:31:09 +0200

    bug fixes by client request

--- src/components/Realtor.jsx
+++ src/components/Realtor.jsx
@@ -1,44 +1,44 @@
 import Image from "next/image";
 import React from "react";
 import useStore from "@/zustand/store/useStore";
+import SocialLinks from "./SocialLinks";
 
 function Realtor() {
   const { language } = useStore();
 
   const isRu = language === "ru";
   return (
-    <div className="flex justify-center items-center text-center">
-      <Image
+    <div className="flex flex-col mx-auto py-2 justify-center items-center text-center">
+      {/* <Image
         className="rounded-tl-md rounded-bl-md"
         src="/images/realtor.webp"
         alt="Realtor Image"
         width={160}
         height={160}
-      />
-      <div className="p-2">
-        <p className="text-sm text-black dark:text-slate-400">
-          {isRu ? "Занимается объектом:" : "Engaged in the object:"}
-        </p>
-        <p className="text-xl font-medium text-black dark:text-slate-100">
-          {isRu ? "Ковалевский Александр" : "Kovalevskiy Alexandr"}
-        </p>
-        {/* <p>
+      /> */}
+      <p className="text-sm text-black dark:text-slate-400">
+        {isRu ? "Занимается объектом:" : "Engaged in the object:"}
+      </p>
+      <p className="text-xl font-medium text-black dark:text-slate-100">
+        {isRu ? "Ковалевский Александр" : "Kovalevskiy Alexandr"}
+      </p>
+      {/* <p>
                     Директор
                 </p> */}
-        <div className="text-center text-sm mt-2 text-black dark:text-slate-100">
-          <a
+      <div className="text-center text-sm mt-1 text-black dark:text-slate-100">
+        {/* <a
             href="mailto:cactusbusines@gmail.com"
             className="hover:text-green-600 transition duration-300 block"
           >
             cactusbusines@gmail.com
-          </a>
-          <a
-            href="tel:+355685577016"
-            className="hover:text-green-600 transition duration-300 text-xl block"
-          >
-            +355 68 557 7016
-          </a>
-        </div>
+          </a> */}
+        <a
+          href="tel:+355685577016"
+          className="hover:text-green-600 transition duration-300 text-xl block mb-1"
+        >
+          +355 68 557 7016
+        </a>
+        <SocialLinks />
       </div>
     </div>
   );
