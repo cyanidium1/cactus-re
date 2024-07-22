7eaa033a1c8dc08b60d4d31465512c96aa2d0203
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-07-22 02:15:56 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,6 +12,25 @@ function Navigation() {
 
   return (
     <ul className="flex flex-col gap-[15px]">
+      <a
+        href="tel:+355685577016"
+        className="dark:text-green-600 dark:fill-green-600 w hover:text-green-600 transition duration-300 text-xl flex mb-1 items-center hover:fill-green-600"
+      >
+        <svg
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="size-5  me-3"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+        </svg> <span>+355 68 557 7016</span>
+      </a>
       <li>
         <Link
           className="w-full text-customGreen text-lg font-normal hover:text-green-400 duration-300 transition-all"
