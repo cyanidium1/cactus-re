60e9d04647371d91d48a3ff59415cd2484507f25
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-07-31 05:45:08 +0200

    translations fixes

--- src/components/PropCard.jsx
+++ src/components/PropCard.jsx
@@ -83,7 +83,7 @@ function PropCard({ el, isGrid, isRU }) {
                 <path d="M20 9.557V3h-2v2H6V3H4v6.557C2.81 10.25 2 11.525 2 13v4a1 1 0 0 0 1 1h1v4h2v-4h12v4h2v-4h1a1 1 0 0 0 1-1v-4c0-1.475-.811-2.75-2-3.443zM18 7v2h-5V7h5zM6 7h5v2H6V7zm14 9H4v-3c0-1.103.897-2 2-2h12c1.103 0 2 .897 2 2v3z"></path>
               </svg>
               <span className="text-base sm:text-xl md:text-sm lg:text-xl">
-                {isRU ? roomsEnglish : roomsRussian}
+                {isRU ? roomsRussian : roomsEnglish}
               </span>
             </li>
             <li className="flex items-center">
@@ -129,12 +129,12 @@ function PropCard({ el, isGrid, isRU }) {
         {!isGrid && (
           <div className="w-1/2 py-6 pr-6 hidden sm:block">
             {isRU
-              ? descriptionEnglish.length > 200
-                ? descriptionEnglish.slice(0, 287) + "..."
-                : descriptionEnglish
-              : descriptionRussian.length > 200
+              ? descriptionRussian.length > 200
                 ? descriptionRussian.slice(0, 287) + "..."
-                : descriptionRussian}
+                : descriptionRussian
+              : descriptionEnglish.length > 200
+                ? descriptionEnglish.slice(0, 287) + "..."
+                : descriptionEnglish}
           </div>
         )}
       </div>
