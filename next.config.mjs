cebe0c5dcb066ba7a72f9075579d2ca5761913f3
Fedir <99473719+cyanidium1@users.noreply.github.com>
2024-12-26 00:19:18 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: true,
     domains: ["www.datocms-assets.com", "cdn.sanity.io"],
   },
   i18n: {
