cef5a9d2edc5f5b840e8f3634b6eef89a0b527f3
Fedir <99473719+cyanidium1@users.noreply.github.com>
2025-07-29 13:03:49 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,26 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103482760', 'ym');
+
+            ym(103482760, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `
+        }}
+      />
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/103482760" style={{position:'absolute', left:'-9999px'}} alt="" />
+        </div>
+      </noscript>
+      <Script
+        strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-0P0PSTJXSX"
       />
       <Script id="google-analytics" strategy="afterInteractive">
